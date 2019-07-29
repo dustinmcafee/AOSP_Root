1aeebfd4739290d88934883bd7218ff7a3d70119
Dustin McAfee <dustin.mcafee@elotouch.com>
2019-07-29 10:40:02 -0400

    Added a logcat command

--- push_termux_files_physical.sh
+++ push_termux_files_physical.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-adb push /home/dustin/AOSP/workingdir/Android_Source_Nougat/update_termux.sh /data/data/com.termux/files/home/ &&
-adb shell chmod 06755 /data/data/com.termux/files/home/update_termux.sh &&
-adb push /home/dustin/AOSP/workingdir/test_event1.cpp /data/data/com.termux/files/home/
+adb push ./update_termux.sh /data/data/com.termux/files/home/ &&
+adb shell chmod 06755 /data/data/com.termux/files/home/update_termux.sh
 
